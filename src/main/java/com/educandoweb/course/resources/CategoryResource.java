82fff7527f421d473268d6fb1bca6ed9f73e7a50
pedrosiufc <pedrosiufc@gmail.com>
2024-09-04 14:02:15 -0300

    Implements Category

---
+++ src/main/java/com/educandoweb/course/resources/CategoryResource.java
@@ -0,0 +1,37 @@
+package com.educandoweb.course.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.services.CategoryService;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+	// Criando um dependência para o service
+	@Autowired // injeção de dependência
+	private CategoryService service;
+
+	// Requisição tipo Get
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = service.findAll();
+
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}") //retornando o caminho dessa variável
+	public ResponseEntity<Category> findById(@PathVariable Long id) {
+		Category obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
