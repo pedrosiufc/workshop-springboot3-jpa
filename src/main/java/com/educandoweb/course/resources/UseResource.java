f953a9bbce5f0d8dc2643d06c96396ec821ee4ef
pedrosiufc <pedrosiufc@gmail.com>
2024-09-04 08:53:58 -0300

    Service layer, component registration

--- src/main/java/com/educandoweb/course/resources/UseResource.java
+++ src/main/java/com/educandoweb/course/resources/UseResource.java
@@ -5,24 +5,35 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
 
+import jakarta.websocket.server.PathParam;
+
 @RestController
 @RequestMapping(value = "/users")
 public class UseResource {
-	
-	//Criando um dependência para o service
-	@Autowired //injeção de dependência
+
+	// Criando um dependência para o service
+	@Autowired // injeção de dependência
 	private UserService service;
-	
+
+	// Requisição tipo Get
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		List<User> list=service.findAll();
-		
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+
 		return ResponseEntity.ok().body(list);
 	}
+
+	@GetMapping(value = "/{id}") //retornando o caminho dessa variável
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
 }
